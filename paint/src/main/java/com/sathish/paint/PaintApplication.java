94e37e4cbfac85c6fd22dc157dfa4bd981280549
Sathish-Neelamani <varunsathish2597@gmail.com>
2025-02-12 00:18:03 +0530

    Added mysql

--- paint/src/main/java/com/sathish/paint/PaintApplication.java
+++ paint/src/main/java/com/sathish/paint/PaintApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaintApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaintApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Inside my start-up business application");
+        SpringApplication.run(PaintApplication.class, args);
+    }
 
 }
