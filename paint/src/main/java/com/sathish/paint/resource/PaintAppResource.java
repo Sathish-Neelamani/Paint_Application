f7dac6411f27393f190c427436427c92ac6cdbde
Sathish-Neelamani <varunsathish2597@gmail.com>
2025-07-07 22:25:52 +0530

    added cicd by using gitla-ci.yml

--- paint/src/main/java/com/sathish/paint/resource/PaintAppResource.java
+++ paint/src/main/java/com/sathish/paint/resource/PaintAppResource.java
@@ -1,9 +1,11 @@
 package com.sathish.paint.resource;
 
+/*import io.swagger.v3.oas.annotations.tags.Tag;*/
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Tag(name = "PAINT")
 @RestController
 @RequestMapping(name = "PAINT APP", path = {"/paint/v1"})
 public class PaintAppResource {
